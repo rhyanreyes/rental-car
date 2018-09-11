a2e086722819ea0b6327d8a7293887f7a42f9a18
Rhyan Reyes <rhyan.reyes@live.com>
2018-09-10 17:09:05 -0700

    Commit for RentalCars with gitignore

---
+++ RentalCarsAPI/RentalCarsAPI/Models/Domain/RentalCar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentalCarsAPI.Models.Domain
+{
+    public class RentalCar
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+    }
+}
