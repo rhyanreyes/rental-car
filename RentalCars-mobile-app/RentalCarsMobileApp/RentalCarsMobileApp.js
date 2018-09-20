759586aa8529f603750c6207e78d22517db47297
Rhyan Reyes <rhyan.reyes@live.com>
2018-09-20 13:57:52 -0700

    Created Example App to check out Shoutem UI theme for React Native

--- RentalCars-mobile-app/RentalCarsMobileApp/RentalCarsMobileApp.js
+++ RentalCars-mobile-app/RentalCarsMobileApp/RentalCarsMobileApp.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import { NavigationBar, Title, ImageBackground } from "@shoutem/ui";
+
 export default class RentalCarsMobileApp extends Component {
   render() {
     return (
@@ -8,6 +10,18 @@ export default class RentalCarsMobileApp extends Component {
         {/* <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text> */}
+        {/* <NavigationBar centerComponent={<Title>Rental Cars</Title>} /> */}
+        <ImageBackground
+          source={{
+            uri: "https://shoutem.github.io/img/ui-toolkit/examples/image-3.png"
+          }}
+          style={{ width: 375, height: 70 }}
+        >
+          <NavigationBar
+            styleName="clear"
+            centerComponent={<Title>Rental Cars</Title>}
+          />
+        </ImageBackground>
         <Text>Rental Cars Mobile App!</Text>
         <Text>First mobile app made by Rhyan Reyes</Text>
         <Text>Another line for the app</Text>
