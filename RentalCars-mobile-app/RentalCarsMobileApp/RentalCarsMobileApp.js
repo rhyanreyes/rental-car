b869b16fc26a7244c6aa5768d7d5ffd9dfe40ed1
Rhyan Reyes <rhyan.reyes@live.com>
2018-09-20 15:55:19 -0700

    Created ExampleApp to demo Shoutem UI theme

--- RentalCars-mobile-app/RentalCarsMobileApp/RentalCarsMobileApp.js
+++ RentalCars-mobile-app/RentalCarsMobileApp/RentalCarsMobileApp.js
@@ -1,31 +1,25 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-import { NavigationBar, Title, ImageBackground } from "@shoutem/ui";
+import { NavigationBar, Title, ImageBackground, Icon } from "@shoutem/ui";
 
 export default class RentalCarsMobileApp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        {/* <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text> */}
-        {/* <NavigationBar centerComponent={<Title>Rental Cars</Title>} /> */}
-        <ImageBackground
-          source={{
-            uri: "https://shoutem.github.io/img/ui-toolkit/examples/image-3.png"
-          }}
-          style={{ width: 375, height: 70 }}
-        >
-          <NavigationBar
-            styleName="clear"
-            centerComponent={<Title>Rental Cars</Title>}
-          />
-        </ImageBackground>
-        <Text>Rental Cars Mobile App!</Text>
-        <Text>First mobile app made by Rhyan Reyes</Text>
-        <Text>Another line for the app</Text>
-      </View>
+      <NavigationBar
+        leftComponent={<Icon name="sidebar" />}
+        centerComponent={<Title>Rental Cars!</Title>}
+      />
+      // <View style={styles.container}>
+      //   {/* <Text>Open up App.js to start working on your app!</Text>
+      //   <Text>Changes you make will automatically reload.</Text>
+      //   <Text>Shake your phone to open the developer menu.</Text> */}
+      //   {/* <NavigationBar centerComponent={<Title>Rental Cars</Title>} /> */}
+
+      //   <Text>Rental Cars Mobile App!</Text>
+      //   <Text>First mobile app made by Rhyan Reyes</Text>
+      //   <Text>Another line for the app</Text>
+      // </View>
     );
   }
 }
